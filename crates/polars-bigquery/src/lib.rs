cc55c54d1d53822532e27ffba2f93dc75b557a27
Tim Sweña (Swast) <tswast@gmail.com>
2026-08-18 16:29:30 -0500

    chore: move errors to an error submodule (#20)

--- crates/polars-bigquery/src/lib.rs
+++ crates/polars-bigquery/src/lib.rs
@@ -1,6 +1,8 @@
 mod bigquery_read_retry;
 mod bigquery_read_stream;
+mod error;
 
+use crate::error::BigQueryError;
 use std::io::Cursor;
 use std::sync::Arc;
 
@@ -16,48 +18,6 @@ use polars_arrow::datatypes::ArrowSchemaRef;
 use polars_arrow::io::ipc::read::read_stream_metadata;
 use polars_arrow::record_batch::RecordBatch;
 use tower::ServiceExt;
-
-#[derive(Debug)]
-pub enum BigQueryError {
-    Grpc(tonic::Status),
-    Arrow(polars_error::PolarsError),
-    Protocol(String),
-    Other(Box<dyn std::error::Error + Send + Sync>),
-}
-
-impl std::fmt::Display for BigQueryError {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match self {
-            Self::Grpc(s) => write!(f, "gRPC transport error: {}", s),
-            Self::Arrow(e) => write!(f, "Arrow decoding error: {}", e),
-            Self::Protocol(msg) => write!(f, "BigQuery protocol error: {}", msg),
-            Self::Other(e) => write!(f, "BigQuery client error: {}", e),
-        }
-    }
-}
-
-impl std::error::Error for BigQueryError {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        match self {
-            Self::Grpc(e) => Some(e),
-            Self::Arrow(e) => Some(e),
-            Self::Protocol(_) => None,
-            Self::Other(e) => Some(e.as_ref()),
-        }
-    }
-}
-
-impl From<tonic::Status> for BigQueryError {
-    fn from(s: tonic::Status) -> Self {
-        Self::Grpc(s)
-    }
-}
-
-impl From<polars_error::PolarsError> for BigQueryError {
-    fn from(e: polars_error::PolarsError) -> Self {
-        Self::Arrow(e)
-    }
-}
 
 #[derive(Clone)]
 pub struct BigQueryReadClientBuilder {
